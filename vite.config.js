b545952c35ea35b8d2407e6ee48c9398daf35c23
alfre4500 <97967070+alfre4500@users.noreply.github.com>
2023-10-16 20:10:58 -0300

    mejoras-2

--- vite.config.js
+++ vite.config.js
@@ -13,7 +13,6 @@ export default {
         emptyOutDir: true,
         rollupOptions: {
             input: {
-                acessorios: resolve('acessorios.html'),
                 celulares: resolve('celulares.html'),
                 inicio: resolve('inicio.html'),
                 index: resolve('index.html'),
